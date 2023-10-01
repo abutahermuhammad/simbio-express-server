705d8a870fe9254c528e46dcf686b6368392445a
abutahermuhammad <abut1081@gmail.com>
2023-10-02 04:59:58 +0600

    Added new configs file for further use

--- configs/server.config.ts
+++ configs/server.config.ts
@@ -6,3 +6,9 @@ dotenv.config();
 export const PORT = process.env.PORT || 5000;
 
 export const NUM_WORKERS = 4;
+
+// This will determine the use of multi-core cluster.
+// Truthy will run only one core. And Falsy value will use all the available cores.
+// Currently using `false` value to prevent `rate limit` related issue message. 
+// It is recommended to use true value for the production.
+export const SINGLE_CORE = true;
