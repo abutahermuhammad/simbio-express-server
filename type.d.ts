bb33de030e99decc04f53aa3cf782133adce8e0f
abutahermuhammad <abut1081@gmail.com>
2023-10-03 16:01:02 +0600

    Added `GET` request parameter types

--- type.d.ts
+++ type.d.ts
@@ -6,3 +6,18 @@ export type ContextOptions = "web" | "tauri" | "app";
 
 // Enum for supported data sorting types.
 export type SortType = "asc" | "dsc";
+
+
+// GET Methods default params
+export type GetRequestParams = {
+    context?: ContextOptions;
+    search?: string;
+    filter?: string;
+    q?: string;
+    limit?: number;
+    offset?: number;
+    sortedBy?: string;
+    sortOrder?: SortType;
+    exclude?: AppId[];
+    include?: AppId[];
+}
