08506c1de8a7d6846ac9b7d8bad913574b15e2b8
abutahermuhammad <abut1081@gmail.com>
2023-10-03 16:01:36 +0600

    Added" `/ambulance`

---
+++ routes/ambulance.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { deleteAmbulance, getAmbulance, getAmbulances, patchAmbulance, postAmbulance } from "../controllers/ambulance.controllers";
+
+// Creating an Express Router instance
+const router = Router();
+
+// Define routes for operations on a collection of members (/members)
+router.route('/ambulance')
+    .get(getAmbulances)    // Fetch a list of members
+    .post(postAmbulance);  // Create a new member
+
+// Define routes for operations on an individual member (/members/:id)
+router.route('/ambulance/:id')
+    .get(getAmbulance)     // Fetch a member by ID
+    .patch(patchAmbulance) // Update a member partially by ID
+    .delete(deleteAmbulance); // Delete a member by ID
+
+// Export the router to make it available for use in other parts of the application
+export default router;
