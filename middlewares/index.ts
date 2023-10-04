88e9b5ff40d8e0348456a56e3df010563a481e92
abutahermuhammad <abut1081@gmail.com>
2023-10-05 01:25:09 +0600

    Updated `Code comments` and `messages`

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -10,27 +10,32 @@ import staticDirMiddleware from "./staticDir.middleware";
 
 // Define an array of middlewares with their corresponding order and descriptions
 const middlewares: { middleware: RequestHandler; description: string }[] = [
-  { middleware: corsMiddleware, description: "CORS Manager." },
-  { middleware: rateLimitMiddleware, description: "Rate-limiter" },
-  { middleware: compressionMiddleware, description: "Response Compressor" },
-  { middleware: jsonMiddleware, description: "JSON Supporter." },
-  { middleware: cookieParserMiddleware, description: "Cookie Parser." },
-  { middleware: cookieSessionMiddleware, description: "Cookie Session." },
-  { middleware: securityHeaderMiddleware, description: "Security Header." },
-  { middleware: staticDirMiddleware, description: "Static Directories." },
+  { middleware: corsMiddleware, description: "CORS Manager." },  // Handles Cross-Origin Resource Sharing (CORS).
+  { middleware: rateLimitMiddleware, description: "Rate-limiter" },  // Enforces rate limiting for requests.
+  { middleware: compressionMiddleware, description: "Response Compressor" },  // Compresses HTTP responses for better efficiency.
+  { middleware: jsonMiddleware, description: "JSON Supporter." },  // Adds JSON support for request and response handling.
+  { middleware: cookieParserMiddleware, description: "Cookie Parser." },  // Parses incoming cookies from requests.
+  { middleware: cookieSessionMiddleware, description: "Cookie Session." }, // Manages session data using cookies.
+  { middleware: securityHeaderMiddleware, description: "Security Header." },  // Enhances security by setting HTTP headers.
+  { middleware: staticDirMiddleware, description: "Static Directories." },  // Serves static files from specified directories.
 ];
 
+
 /**
- * Middleware Initializer
+ * Initialize Middlewares
  * @param app Express application instance
+ * 
+ * This function initializes and adds various middleware functions to the Express app.
+ * Each middleware is added in a specific order to handle various aspects of HTTP requests.
  * 
  * @since 1.0.0
  */
 export function initializeMiddlewares(app: Express) {
   // Use all middlewares in the defined order
-  console.log(`Initialized middlewares`);
+  console.log("Initializing middlewares...");
   middlewares.forEach(({ middleware, description }) => {
     app.use(middleware);
-    console.log(`\t${description}`);
+    console.log(`\tLoaded: ${description}`);
   });
+  console.log("Middlewares initialized successfully. \n");
 }
