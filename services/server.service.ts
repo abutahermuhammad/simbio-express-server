0720e12ce54f7f7ebbd82d09def4b08f5b840882
abutahermuhammad <abut1081@gmail.com>
2023-10-05 01:19:03 +0600

    Updated `comments` and `Messages`

--- services/server.service.ts
+++ services/server.service.ts
@@ -2,7 +2,9 @@ import cluster from "cluster";
 import { Express } from "express";
 import { NUM_WORKERS, PORT } from "../configs/server.config";
 
-const makeServer = (app: Express) => {
+// Function to initialize the server
+const initializeServer = (app: Express) => {
+    // Check if this is the primary (master) process
     if (cluster.isPrimary) {
         console.log(`Master process ${process.pid} is running`);
 
@@ -11,49 +13,37 @@ const makeServer = (app: Express) => {
             cluster.fork();
         }
 
-        // Handle graceful shutdown
+        // Handle graceful shutdown when receiving SIGTERM signal
         process.on('SIGTERM', () => {
             console.log('Master process is shutting down...');
             for (const id in cluster.workers) {
-                cluster?.workers[id]?.kill();
+                cluster.workers[id]?.kill();
             }
         });
 
-        // Listen for dying worker processes and fork new ones
+        // Listen for worker processes that exit unexpectedly and fork new ones
         cluster.on('exit', (worker, code, signal) => {
             console.log(`Worker process ${worker.process.pid} died. Restarting...`);
             cluster.fork();
-            console.log(`CODE: ${code}\t  SIGNAL: ${signal}`);
+            console.log(`Exit Code: ${code}\t  Signal: ${signal}`);
         });
     } else {
-        // Each worker process runs the app
+        // Each worker process runs the Express app and listens on the specified PORT
         const server = app.listen(PORT, () => {
             console.log(`Worker process ${process.pid} started. Listening on port ${PORT}`);
         });
 
-        cluster?.worker?.on('disconnect', () => {
-            console.log(`Worker process ${cluster?.worker?.id} is disconnecting...`);
+        // Handle worker disconnect and exit events
+        cluster.worker?.on('disconnect', () => {
+            console.log(`Worker process ${cluster.worker?.id} is disconnecting...`);
         });
 
-        cluster?.worker?.on('exit', (code, signal) => {
-            console.log(`Worker process ${cluster?.worker?.id} has exited.`);
+        cluster.worker?.on('exit', (code, signal) => {
+            console.log(`Worker process ${cluster.worker?.id} has exited.`);
         });
     }
 };
 
-
-/**
- * Server Initializer
- * @param app
- * 
- * @since 1.0.0
- */
-const initializeServer = (app: Express) => {
-    makeServer(app);
-};
-
-
-
-// Exporting modules
+// Export the server initialization function
 export { initializeServer };
 
