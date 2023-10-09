9d8e59182b7aa6f889ce929c35cd2c78f811f072
abutahermuhammad <abut1081@gmail.com>
2023-10-09 12:22:35 +0600

    Added `options` to `JSON` middleware

--- configs/server.config.ts
+++ configs/server.config.ts
@@ -13,3 +13,6 @@ export const PORT = process.env.PORT || 5000;
 // export const NUM_WORKERS: number = os.cpus().length || 1;
 export const NUM_WORKERS: number = ENVIRONMENT === "development" ? 1 : os.cpus().length;
 
+// Request Payload Size
+// This will define the allowed payload size for a upcoming payload withing a REST API call.
+export const MAX_REQUEST_PAYLOAD_SIZE = '100kb';
