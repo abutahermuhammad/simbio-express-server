e5837598acd9ae996069fa923705ed31596b9e23
abutahermuhammad <abut1081@gmail.com>
2023-10-12 01:30:06 +0600

    Updated `log` middleware

--- middlewares/logger.middleware.ts
+++ middlewares/logger.middleware.ts
@@ -6,6 +6,4 @@ import morgan from 'morgan';
  * This middleware logs incoming requests and responses to the console.
  * You can customize the log format according to your preferences.
  */
-export const loggerMiddleware = morgan('dev');
-
-// Apply the logger middleware to your Express app.
+export default morgan('dev');
