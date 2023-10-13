078769630c69ddf056a25a0599ac4a424021f120
abutahermuhammad <abut1081@gmail.com>
2023-10-14 00:29:35 +0600

    Updated app `config`

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
+import config from 'config';
 import express, { Express, NextFunction, Request, Response } from 'express';
 import { initializeMiddlewares } from './middlewares';
 import { initializeRoutes } from './routes';
 import { initializeServer } from './services/server.service';
+// import debug from ('debug')('app:startup');
 
+
+// debug("Application started")
 
 // Create an Express application instance
+// file deepcode ignore DisablePoweredBy: <Already added `helmet` module in the middleware script>
+// deepcode ignore UseCsurfForExpress: <please specify a reason of ignoring this>
 const app: Express = express();
-
+app.set('host', config.get("server.ip"));
+app.set('port', config.get('server.port'));
+app.set('trust proxy', 1) // trust first proxy
+app.disable('x-powered-by');
 // Disable the trust proxy setting for security reasons
-app.set('trust proxy', false);
+app.disable('trust proxy')
 
 // Initialize middlewares for the Express app
 initializeMiddlewares(app);
@@ -17,11 +26,14 @@ initializeMiddlewares(app);
 initializeRoutes(app);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Can't find ${req.originalUrl} on the server!`,
-    })
-    // next()
+    try {
+        res.status(404).json({
+            status: "fail",
+            message: `Can't find ${req.originalUrl} on the server!`,
+        })
+    } catch (error) {
+        next(error)
+    }
 });
 
 /**
@@ -33,3 +45,5 @@ app.use((req: Request, res: Response, next: NextFunction) => {
  * @since 1.0.0
  */
 initializeServer(app);
+
+
