933c814879fe5c7b866038cebdc197ff5961ba42
abutahermuhammad <abut1081@gmail.com>
2023-11-08 00:35:28 +0600

    fix(session middleware): add options type gaurd

--- middlewares/session.middleware.ts
+++ middlewares/session.middleware.ts
@@ -1,24 +1,23 @@
-import session from 'express-session';
-import crypto from 'crypto'
+import session, { SessionOptions } from 'express-session';
 
 /**
  * Cookie Session Configuration
- * 
+ *
  * This middleware sets up a cookie-based session with the specified options.
  * It includes a session name, secret keys for encryption, and cookie options
  * such as the maximum age of the cookie.
- * 
+ *
  * For more details check  out [documentation](https://expressjs.com/en/resources/middleware/session.html)
  */
 
-const options = {
+const options: SessionOptions = {
   resave: true,
   saveUninitialized: true,
-  secret: "" ,
+  secret: "",
   cookie: {
-    httpOnly: true,
-    sameSite: true,
-    //   maxAge: 24 * 60 * 60 * 1000, // Maximum age of the session cookie (24 hours)
+    httpOnly: false,
+    sameSite: false,
+    maxAge: 24 * 60 * 60 * 1000, // Maximum age of the session cookie (24 hours)
   },
 
 }
