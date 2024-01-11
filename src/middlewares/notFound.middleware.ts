ee5d5195265f1180f457007a7e25794b795d9d90
abutahermuhammad <abut1081@gmail.com>
2024-01-11 06:26:33 +0600

    refactor!

--- src/middlewares/notFound.middleware.ts
+++ src/middlewares/notFound.middleware.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import sendResponse from './../utils/sendResponse.util';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(httpStatus.NOT_FOUND).json({
-        success: false,
+    return sendResponse(res, {
+        statusCode: httpStatus.NOT_FOUND,
         message: 'API Not Found!',
-        error: ""
-    });
+
+    })
 };
 
 export default notFound;
--- src/utils/sendResponse.util.ts
+++ src/utils/sendResponse.util.ts
@@ -2,18 +2,18 @@ import { Response } from 'express';
 
 type TResponse<UserData, UserError> = {
     statusCode: number;
-    success: boolean;
-    message?: string;
+    success?: boolean;
+    message: string;
     data?: UserData;
     error?: UserError;
 };
 
 const sendResponse = <UserData, UserError>(res: Response, data: TResponse<UserData, UserError>) => {
     res.status(data?.statusCode).json({
-        success: data.success,
+        success: data.success || false,
         message: data.message,
         data: data?.data || {},
-        error: data?.error || null,
+        error: data?.error || [],
     });
 };
 
